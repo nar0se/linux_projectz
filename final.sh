6bc1ef8f21f3e8c191ef7e40e649958326696f6c
nar0se <hollowgemini@gmail.com>
2021-04-13 18:32:34 -0400

    Tax changes

--- final.sh
+++ final.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
-#read -p "Enter customers name " cust
-#sudo useradd $cust 
-#if [ $? -ne 0 ];
-#then
-#echo " You already have an account with us so ur eligible for 20% discount"
-#exit 1
-#else
-#echo " new account created "
-#read -p "enter addr" addr
-#read -p "enter phone num " phone
-#fi
+
+function showGraphic() {
+	echo "
+	echo "
+	echo "
+	echo "
+	echo "
+	echo "
+	echo "
+	echo "
+	echo "
+	echo "
+	echo "
+	echo "
+	echo "
+	echo "
+	echo "
+	echo "
+
+}
+
+read -p "Enter customers name " cust
+read -p "Enter address " addr
+read -p "Enter phone number " phone
+echo "Account successfully created"
 
 # function main
 main() 
@@ -22,7 +36,7 @@ done
 read option
 if [ $option -le 2 ] && [ $option -ge 0 ];
 then
-echo "Happy shopping"
+echo "Our food is always fresh."
 else
 echo " Invalid option : Try again "
 fi
@@ -34,15 +48,15 @@ read -p "press 1 to continue shopping 2 to checkout" out
 if [ $out -eq 1 ]; then
 main
 else
-echo "check out"
+echo "Check Out"
 echo "-------------------------"
-echo "Total items purchased $(cat new.txt | wc -l)"
+echo "Total items purchased: \$$(cat new.txt | wc -l)"
 echo "-------------------------"
 echo "$(cat new.txt)"
 echo "--------------------------"
-echo "total cost = $(awk '{s+=$2}END{print s}' new.txt)"
+echo "Subtotal: \$$(awk '{s+=$2}END{print s}' new.txt)"
 echo "--------------------------"
-echo "total price after 2.5% tax = $(awk '{s+=$2}END{print (s*0.025)+s}' new.txt)" 
+echo "Total price with 3% tax: \$$(awk '{s+=$2}END{print $(s*0.03)+s}' new.txt)" 
 echo "---------------------------"
 
 
@@ -63,10 +77,16 @@ do
 while [ "$option" -eq 0 ]
 do
 declare -a array=( "Apple 1.25" "tomatoes 2.54" "spinach 4.56" "carrot 0.98" )
-read -p " press 0:Apple 1:tomatoes 2:spinach 3:carrot " var 
+printf "Please select a choice.\n0:Apple\n1:Tomatoes\n2:Spinach\n3:Carrot "
+read var
+if [ $var -gt 3 ]
+then
+echo "Invalid choice."
+break
+fi
 for i in "${array[*]}"
 do
- echo "${array[$var]} added to cart"
+echo "${array[$var]} added to cart"
 echo "${array[$var]}" >> new.txt
 break
 done
@@ -75,11 +95,16 @@ done
 
 
 #options for Diary while true
-
 while [ "$option" -eq 1 ]
 do
 declare -a diary=( "milk 3.25" "cheese 5.67" "eggs 6.87" "yogurt 7.34" )
-read -p "press 0:milk 1:cheese  2:eggs 3:yogurt " key
+printf "Please select a choice.\n0:Milk\n1:Cheese\n2:Eggs\n3:Yogurt "
+read key
+if [ $key -gt 3 ]
+then
+echo "Invalid choice."
+break
+fi
 for i in "${diary[*]}"
 do
 echo "${diary[$key]} added to cart "
@@ -88,11 +113,17 @@ break
 done
 break
 done
+
  #options for meat
 while [ "$option" -eq 2 ]
 do
-declare -a meat=( "chicken 6.45" "salmon 12.56" "shrimp 8.67")
-read -p "press 0:chicken 1:salmon 2:shrimp " rt
+declare -a meat=( "chicken 6.45" "salmon 12.56" "shrimp 8.67" )
+printf "Please select a choice.\n0:Chicken\n1:Salmon\n2:Shrimp "
+read rt
+if [ $rt -gt 3 ]
+then
+echo "Invalid choice."
+fi
 for i in "${meat[*]}"
 do
 echo "${meat[$rt]} added to cart"
@@ -103,10 +134,4 @@ break
 done
 shop $out
 done
-#echo "check out"
-#echo "Total items purchased $(cat new.txt | wc -l)"
-#echo "$(cat new.txt)"
-#echo "total cost is"
-#echo "-----------------------"
-#awk '{s+=$2}END{print s}' new.txt
 
